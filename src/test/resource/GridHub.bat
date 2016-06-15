50fba303e737d61b952f8bdd62d23376425066c8
kathir-testautomation <catchkathirk@gmail.com>
2016-06-15 13:52:08 +0530

    updated grid bat file

--- src/test/resource/GridHub.bat
+++ src/test/resource/GridHub.bat
@@ -1,3 +1,3 @@
 @echo OFF
-cd %HOMEPATH%\.m2\repository\org\seleniumhq\selenium\selenium-server-standalone\2.53.0
+cd C:\Users\Kathir\.m2\repository\org\seleniumhq\selenium\selenium-server-standalone\2.53.0
 java -jar selenium-server-standalone-2.53.0.jar -role hub -port 4444
