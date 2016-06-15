54c9348b2e12d438c834f047196283fd66aec9f1
Kathiresan K <Kathiresan K>
2016-06-15 17:32:28 +0530

    updated the grid bat files to effect folder switch

--- src/test/resource/GridNode1.bat
+++ src/test/resource/GridNode1.bat
@@ -1,3 +1,2 @@
-@echo OFF
-cd C:\Users\Kathir\.m2\repository\org\seleniumhq\selenium\selenium-server-standalone\2.53.0
+cd /D C:\Users\Kathir\.m2\repository\org\seleniumhq\selenium\selenium-server-standalone\2.53.0
 java -jar selenium-server-standalone-2.53.0.jar -role node -Dwebdriver.chrome.driver="E:/Study/Softwares/chromedriver.exe" -hub http://localhost:4444/grid/register -port 5566 -browser browserName=chrome,platform=WINDOWS,maxInstances=2 -maxSession 1 -timeout 200000
--- src/test/resource/GridNode2.bat
+++ src/test/resource/GridNode2.bat
@@ -1,3 +1,2 @@
-@echo OFF
-cd C:\Users\Kathir\.m2\repository\org\seleniumhq\selenium\selenium-server-standalone\2.53.0
+cd /D C:\Users\Kathir\.m2\repository\org\seleniumhq\selenium\selenium-server-standalone\2.53.0
 java -jar selenium-server-standalone-2.53.0.jar -role node -hub http://localhost:4444/grid/register -port 5577 -browser browserName=firefox,platform=WINDOWS,maxInstances=3 -maxSession 1 -timeout 200000
