31e07431c46edf13a34e80f7779975c20463e846
Kathiresan K <Kathiresan K>
2016-06-15 13:40:35 +0530

    changed bat files to fix home directory command

--- src/test/resource/GridNode1.bat
+++ src/test/resource/GridNode1.bat
@@ -1,3 +1,3 @@
 @echo OFF
-cd %~%\.m2\repository\org\seleniumhq\selenium\selenium-server-standalone\2.53.0
+cd %HOMEPATH%\.m2\repository\org\seleniumhq\selenium\selenium-server-standalone\2.53.0
 java -jar selenium-server-standalone-2.53.0.jar -role node -Dwebdriver.chrome.driver="E:/Study/Softwares/chromedriver.exe" -hub http://localhost:4444/grid/register -port 5566 -browser browserName=chrome,platform=WINDOWS,maxInstances=2 -maxSession 1 -timeout 200000
