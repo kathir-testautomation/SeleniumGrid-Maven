567abde3685ddb5705527eb611c340b9b5a4f242
Kathiresan K <Kathiresan K>
2016-06-15 13:33:43 +0530

    changed grid bat files to access selenium jar from Local Maven repositoy

--- src/test/resource/GridHub.bat
+++ src/test/resource/GridHub.bat
@@ -1,3 +1,3 @@
 @echo OFF
-cd E:\Study\Softwares\Jar\Selenium
+cd %~%\.m2\repository\org\seleniumhq\selenium\selenium-server-standalone\2.53.0
 java -jar selenium-server-standalone-2.53.0.jar -role hub -port 4444
--- src/test/resource/GridNode1.bat
+++ src/test/resource/GridNode1.bat
@@ -1,3 +1,3 @@
 @echo OFF
-cd E:\Study\Softwares\Jar\Selenium
+cd %~%\.m2\repository\org\seleniumhq\selenium\selenium-server-standalone\2.53.0
 java -jar selenium-server-standalone-2.53.0.jar -role node -Dwebdriver.chrome.driver="E:/Study/Softwares/chromedriver.exe" -hub http://localhost:4444/grid/register -port 5566 -browser browserName=chrome,platform=WINDOWS,maxInstances=2 -maxSession 1 -timeout 200000
